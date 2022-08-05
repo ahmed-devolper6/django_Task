e890da0d2dcee599d51f5ef6c0c346332debb4a7
jamkia01010 <ahmedjamal100@gmail.com>
2022-08-06 01:52:08 +0200

    add url models temaplates and view for posts

---
+++ posts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class posts(models.Model):
+    name = models.CharField(max_length=100)
+    contat = models.TextField(max_length=100)
+    def __str__(self) -> str:
+        return self.name
